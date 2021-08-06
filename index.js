6f82c3f5a39a1e24fcf22621c73ffaa91fc855a1
Sahatsarin Pawanna <sahatsarin07@gmail.com>
2021-08-06 17:35:47 +0700

    Add Cors

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const project = require('./routes/project');
 const connect = require('./config/db');
+const cor = require('cors');
 
 const PORT = 4000;
 
 const app = express();
 connect();
 
+app.use(cor());
 // body parser
 app.use(express.json({ extended: false }));
 
